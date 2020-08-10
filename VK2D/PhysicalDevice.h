68529576465db8571d21b79f56f348ec319c393e
pdmazzon@gmail.com <pdmazzon@gmail.com>
2020-08-10 16:28:18 -0700

    Improved python script and added some basic physical device things

--- VK2D/PhysicalDevice.h
+++ VK2D/PhysicalDevice.h
@@ -2,3 +2,17 @@
 /// \author Paolo Mazzon
 /// \brief Tools for managing VkPhysicalDevice
 #pragma once
+#include <vulkan/vulkan.h>
+
+/// \brief Groups up a couple things related to VkPhysicalDevice
+struct VK2DPhysicalDevice {
+	VkPhysicalDevice pd; ///< Internal vulkan pointer
+	struct {
+		uint32_t presentFamily;  ///< KHR family
+		uint32_t graphicsFamily; ///< Queue family for graphics pipeline
+		uint32_t computeFamily;  ///< Queue family for compute pipeline
+	} QueueFamily;               ///< Nicely groups up queue families
+	VkPhysicalDeviceMemoryProperties mem; ///< Memory properties of this device
+	VkPhysicalDeviceFeatures feats;       ///< Features of this device
+	VkPhysicalDeviceProperties props;     ///< Device properties
+};
--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "VK2D/VK2D.h"
+#include <stdio.h>
 
 int main(int argc, const char *argv[]) {
+	printf("Hello, world!");
 	return 0;
 }
