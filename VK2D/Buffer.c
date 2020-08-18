7940dee256172416f7836cbc7bd5d65ef67231ef
pdmazzon@gmail.com <pdmazzon@gmail.com>
2020-08-18 08:59:39 -0700

    Added buffers

--- VK2D/Buffer.c
+++ VK2D/Buffer.c
@@ -1,3 +1,55 @@
 /// \file Buffer.c
 /// \author Paolo Mazzon
 #include "VK2D/Buffer.h"
+#include "VK2D/LogicalDevice.h"
+#include "VK2D/PhysicalDevice.h"
+#include "VK2D/Validation.h"
+#include "VK2D/Initializers.h"
+#include <malloc.h>
+
+uint32_t _reVulkanBufferFindMemoryType(VkPhysicalDeviceMemoryProperties *memProps, uint32_t memoryFilter, VkMemoryPropertyFlags requirements) {
+	uint32_t i;
+	for (i = 0; i < memProps->memoryTypeCount; i++)
+		if (memoryFilter & (1 << i) && (memProps->memoryTypes[i].propertyFlags & requirements) == requirements)
+			return i;
+	vk2dErrorCheck(-1);
+	return UINT32_MAX;
+}
+
+VK2DBuffer vk2dBufferCreate(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags mem, VK2DLogicalDevice dev) {
+	VK2DBuffer buf = malloc(sizeof(struct VK2DBuffer));
+
+	if (vk2dPointerCheck(buf)) {
+		buf->dev = dev;
+		buf->size = size;
+		VkBufferCreateInfo bufferCreateInfo = vk2dInitBufferCreateInfo(size, usage, &dev->pd->QueueFamily.graphicsFamily, 1);
+		vk2dErrorCheck(vkCreateBuffer(dev->dev, &bufferCreateInfo, VK_NULL_HANDLE, &buf->buf));
+
+		VkMemoryRequirements memoryRequirements;
+		vkGetBufferMemoryRequirements(dev->dev, buf->buf, &memoryRequirements);
+
+		VkMemoryAllocateInfo memoryAllocateInfo = vk2dInitMemoryAllocateInfo(memoryRequirements.size, _reVulkanBufferFindMemoryType(&dev->pd->mem, memoryRequirements.memoryTypeBits, mem));
+		vk2dErrorCheck(vkAllocateMemory(dev->dev, &memoryAllocateInfo, VK_NULL_HANDLE, &buf->mem));
+		vkBindBufferMemory(dev->dev, buf->buf, buf->mem, 0);
+	}
+
+	return buf;
+}
+
+void vk2dBufferCopy(VK2DBuffer src, VK2DBuffer dst, uint32_t pool) {
+	VkCommandBuffer buffer = vk2dLogicalDeviceGetSingleUseBuffer(src->dev, pool);
+	VkBufferCopy copyRegion = {};
+	copyRegion.size = src->size;
+	copyRegion.dstOffset = 0;
+	copyRegion.srcOffset = 0;
+	vkCmdCopyBuffer(buffer, src->buf, dst->buf, 1, &copyRegion);
+	vk2dLogicalDeviceSubmitSingleBuffer(src->dev, buffer, pool);
+}
+
+void vk2dBufferFree(VK2DBuffer buf) {
+	if (buf != NULL) {
+		vkFreeMemory(buf->dev->dev, buf->mem, VK_NULL_HANDLE);
+		vkDestroyBuffer(buf->dev->dev, buf->buf, VK_NULL_HANDLE);
+		free(buf);
+	}
+}
--- VK2D/Buffer.h
+++ VK2D/Buffer.h
@@ -2,10 +2,39 @@
 /// \author Paolo Mazzon
 /// \brief Abstraction of VkBuffer to make handling them a little simpler
 #pragma once
+#include <vulkan/vulkan.h>
+#include "VK2D/Structs.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/// \brief Makes managing buffers in Vulkan simpler
+struct VK2DBuffer {
+	VkBuffer buf;          ///< Internal Vulkan buffer
+	VkDeviceMemory mem;    ///< Memory for the buffer
+	VK2DLogicalDevice dev; ///< Device the buffer belongs to
+	VkDeviceSize size;     ///< Size of this buffer in bytes
+};
+
+/// \brief Creates a new buffer with memory and all that ready
+/// \param size Size in bytes of the new buffer
+/// \param usage How the buffer will be used
+/// \param mem Required memory properties (device local, host visible, etc...)
+/// \param dev Device to get the memory from (will use graphics indices)
+/// \return Returns the new buffer or NULL if it failed
+VK2DBuffer vk2dBufferCreate(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags mem, VK2DLogicalDevice dev);
+
+/// \brief Copies the entire contents of src into dst
+/// \param src Buffer to copy from
+/// \param dst Buffer to copy to
+/// \param commandPool Index of the command pool to use on the previously assigned device
+/// \warning Both buffers must originate from the same device
+void vk2dBufferCopy(VK2DBuffer src, VK2DBuffer dst, uint32_t pool);
+
+/// \brief Frees a buffer from memory
+/// \param buf Buffer to free
+void vk2dBufferFree(VK2DBuffer buf);
 
 #ifdef __cplusplus
 };
