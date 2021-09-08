566c8e53bae8d1e43eb37177e88de106e9ab3aa7
Paolo Mazzon <pdmazzon@gmail.com>
2021-09-07 17:43:37 -0700

    only one function left to write

--- VK2D/Camera.c
+++ VK2D/Camera.c
@@ -1,21 +1,33 @@
 /// \file Camera.c
 /// \author Paolo Mazzon
 #include "VK2D/Camera.h"
+#include "VK2D/Renderer.h"
+#include "VK2D/Buffer.h"
 
 VK2DCameraIndex vk2dCameraCreate(VK2DCameraSpec spec) {
 	// TODO: Check renderer's list of cameras for an available spot and place the camera there, creating necessary UBOs as well
+	VK2DRenderer gRenderer = vk2dRendererGetPointer();
 	return 0;
 }
 
 void vk2dCameraUpdate(VK2DCameraIndex index, VK2DCameraSpec spec) {
-	// TODO: Access renderer camera list, copy spec into the camera's internal spec
+	VK2DRenderer gRenderer = vk2dRendererGetPointer();
+	memcpy(&gRenderer->cameras[index].spec, &spec, sizeof(VK2DCameraSpec));
 }
 
 void vk2dCameraSetState(VK2DCameraIndex index, VK2DCameraState state) {
-	// TODO: Either just change state or delete necessary data as well
+	VK2DRenderer gRenderer = vk2dRendererGetPointer();
+	if (state == cs_Deleted || state == cs_Reset) { // Free internal resources
+		for (int i = 0; i < gRenderer->swapchainImageCount; i++)
+			vk2dBufferFree(gRenderer->cameras[index].buffers[i]);
+		free(gRenderer->cameras[index].ubos);
+		free(gRenderer->cameras[index].buffers);
+		free(gRenderer->cameras[index].uboSets);
+	}
+	gRenderer->cameras[index].state = state;
 }
 
 VK2DCameraState vk2dCameraGetState(VK2DCameraIndex index) {
-	// TODO: Access renderer camera list, return camera state
-	return 0;
+	VK2DRenderer gRenderer = vk2dRendererGetPointer();
+	return gRenderer->cameras[index].state;
 }
--- VK2D/Camera.h
+++ VK2D/Camera.h
@@ -17,7 +17,7 @@ struct VK2DCamera {
 	VkViewport viewport;           ///< Where to draw on the screen
 	VK2DCameraSpec spec;           ///< Info on how to create the UBO and scissor/viewport
 	VK2DUniformBufferObject *ubos; ///< UBO data for each frame
-	VK2DBuffer *buffer;            ///< Used for updating the UBOs
+	VK2DBuffer *buffers;           ///< Used for updating the UBOs
 	VkDescriptorSet *uboSets;      ///< List of UBO sets, 1 per swapchain image
 	VK2DCameraState state;         ///< State of this camera
 };
