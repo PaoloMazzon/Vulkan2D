6e5bd931c107683042a6891d1e46a90685fd6f0e
PaoloMazzon <PaoloMazzon@users.noreply.github.com>
2026-01-25 01:40:05 +0000

    deploy: 8e2f52de317310306902a012995613ca2e4fa7c2

--- DescriptorControl_8h.js
+++ DescriptorControl_8h.js
@@ -6,5 +6,6 @@ var DescriptorControl_8h =
     [ "vk2dDescConGetSamplerBufferSet", "DescriptorControl_8h.html#a516c605ac3dff039ed14a439c8376858", null ],
     [ "vk2dDescConGetSamplerSet", "DescriptorControl_8h.html#a7ac118822642a9770588ed16c42104b8", null ],
     [ "vk2dDescConGetSet", "DescriptorControl_8h.html#a9b9b277f3cbb9304a5f5bbf26b9e5076", null ],
-    [ "vk2dDescConReset", "DescriptorControl_8h.html#aec9585994746dcfd82f83421ff50ba6a", null ]
+    [ "vk2dDescConReset", "DescriptorControl_8h.html#aec9585994746dcfd82f83421ff50ba6a", null ],
+    [ "vk2dDescConUpdateAfterBind", "DescriptorControl_8h.html#a3276ed166e02f91e291c1c37a3e841f1", null ]
 ];
